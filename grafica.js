1e9a20101ed4f33f45df9f766130f62c5936e9f9
LuisFOsG <luisfernandogarciaosorio@gmail.com>
2020-11-01 16:17:15 -0500

    SIncronizando Datos con Formulario

--- grafica.js
+++ grafica.js
@@ -1,106 +1,58 @@
-function ajustarDatos(data) {
-    try {
-        let datos = leerCsv(data);
-        if(datos){
-            estanciarDatos(datos);
-        }
-    } catch (e) {
-        console.log(`Error: ${e.message}`);
-    }
-}
-
-function leerCsv(texto, separador = ",", omitirEncabezado=false) {
-    if (typeof texto !== "string") {
-        throw TypeError("El argumento debe ser una cadena de texto");
-    }
-    return texto.slice(omitirEncabezado ? texto.indexOf("\n") + 1 : 0)
-    .split("\n")
-    .map(l => l.split(separador))
+window.onload = function (){
+    $.ajax({
+        url: "Encuesta.csv",
+        dataType: "text",
+        contentType: "charset-utf-8",
+    }).done(ajustarDatos);
 }
 
 function estanciarDatos(datos){
-    let pregunta1 = [];
-    let pregunta2 = [];
-    let pregunta3 = [];
-    let pregunta4 = [];
-    let memoria = 0;
+    let pregunta = [];
+    let respuesta = [];
 
-    /* Quitamos el primer valor de cada arreglo ya que no es necesario */
+    /* Limpia los datos, quita comillas dobles y dato no necesario */
     for(let i=0;i<datos.length;i++){
         datos[i].shift();
     }
-
-    /* Quitamos las comillas que venian por defecto en el titulo */
-    for(let i=0;i<datos[0].length;i++){
-        datos[0][i] = datos[0][i].replaceAll('"', "");
-    }
-
-    /* limpiamos, separamos datos y los colocamos en un vector */
-    for(let i=1;i<datos.length;i++){
-        memoria = (datos[i][0].split("Opción ").pop()).split('"').shift();
-        pregunta1.push(memoria);
-    }
-    /* limpiamos, separamos datos y los colocamos en un vector */
-    for(let i=1;i<datos.length;i++){
-        memoria = (datos[i][1].split("Opción ").pop()).split('"').shift();
-        pregunta2.push(memoria);
-    }
-    /* colocamos los datos en un vector y quitamos las comillas */
-    for(let i=1;i<datos.length;i++){
-        datos[i][2] = datos[i][2].replaceAll('"', "");
-        pregunta3.push(datos[i][2]);
-    }
-    /* colocamos los datos en un vector y quitamos las comillas */
-    for(let i=1;i<datos.length;i++){
-        datos[i][3] = datos[i][3].replaceAll('"', "");
-        pregunta4.push(datos[i][3]);
-    }
-
-    /* IMPRIMIENDO GRAFICAS ===================================================== */
-
-    let ctx = document.getElementById("myChart").getContext("2d");
-    let tipo = "bar";
-    opcionMultiple(ctx, tipo, datos[0][0], pregunta1);
-
-    ctx = document.getElementById("chart").getContext("2d");
-    tipo = "doughnut";
-    opcionMultiple(ctx, tipo, datos[0][1], pregunta2);
-
-    ctx = document.getElementById("edad").getContext("2d");
-    tipo = "bar";
-    opcionMultiple(ctx, tipo, datos[0][2], pregunta3);
-
-    ctx = document.getElementById("hambre").getContext("2d");
-    tipo = "pie";
-    opcionMultiple(ctx, tipo, datos[0][3], pregunta4);
-
-    media("media", pregunta1);
-    media("media1", pregunta2);
-    media("media2", pregunta3);
-}
-
-function media(ctx, datos){
-    let suma = 0;
-    let resultado = "$$ \\overline{x} = \\frac{";
-    let final = "}  $$";
-
     for(let i=0;i<datos.length;i++){
-        resultado += datos[i].toString();
-        suma += parseInt(datos[i]);
-        if(i<datos.length-1){
-            resultado += "+";
-        }else{
-            resultado += "}{"+datos.length;
+        for(let j=0;j<datos[i].length;j++){
+            datos[i][j] = datos[i][j].replaceAll('"', "");
         }
     }
-    suma = suma/datos.length;
-    resultado += final;
-    resultado += "$$ \\overline{x} = ";
-    resultado += suma;
-    resultado += "  $$";
 
-    const math = document.getElementById(ctx);
-    math.innerHTML = resultado;
+    /* Ingresa todas las preguntas en un array */
+    for(let i=0; i<datos[0].length; i++){
+        pregunta.push(datos[0][i]);
+    }
+
+    /* Cada respuesta queda en un array */
+    let arrayMemoria = [];
+    for(let i=0; i<datos[1].length; i++){
+        for(let j=1;j<datos.length;j++){
+            arrayMemoria.push(datos[j][i]);
+        }
+        respuesta.push(arrayMemoria);
+        arrayMemoria = [];
+    }
+
+    /* Imprimiendo Respuestas */
+    /* let txt = "";
+    for(let i=0;i<respuesta.length;i++){
+        txt += respuesta[i];
+        txt += "<br>";
+    }
+    const app = document.getElementById("app");
+    app.innerHTML = txt; */
+
+    respuesta[1] = convertirNumInt(respuesta[1]);
+    respuesta[6] = convertirNumInt(respuesta[6]);
+
+    console.log(pregunta);
+    console.log(respuesta);
+
+    let ctx = "genero";
+    let tipo = "bar";
+    opcionMultiple(ctx, tipo, pregunta[0], respuesta[0]);
 }
 
 function opcionMultiple(ctx, tipo, titulo, datos){
@@ -171,6 +123,7 @@ function opcionMultiple(ctx, tipo, titulo, datos){
         }
     }
 
+    ctx = document.getElementById(ctx).getContext("2d");
     const chart = new Chart(ctx, {
         type: tipo,
         data: {
